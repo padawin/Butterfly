c4015e722b5c9d775f9e214371aaed765df81419
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2012-01-01 14:09:18 +0100

    strtourl simplified

--- View.php
+++ View.php
@@ -173,7 +173,7 @@ class Butterfly_View
         if (is_array($get)) {
             $getString = array();
             foreach ($get as $param => $value) {
-                $getString[] = $param . '/' . $value;
+                $getString[] = $this->strToUrl($param) . '/' . $this->strToUrl($value);
             }
             $getString = implode('/', $getString);
         }
@@ -197,7 +197,7 @@ class Butterfly_View
             return $url;
         }
         else {
-            return strtolower($this->strToUrl(rtrim($url, '/') . ($getString != '' ? '/' . $getString : '' )));
+            return strtolower(rtrim($url, '/') . ($getString != '' ? '/' . $getString : '' ));
         }
     }
 
@@ -208,20 +208,6 @@ class Butterfly_View
 
     public function strToUrl($string)
     {
-        $normalizeChars = array(
-            'Š'=>'S', 'š'=>'s', 'Ð'=>'Dj','Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A',
-            'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E', 'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I',
-            'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U',
-            'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss','à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a',
-            'å'=>'a', 'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i',
-            'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u',
-            'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y', 'ƒ'=>'f'
-        );
-
-        $string     =    str_replace(' ', '-', $string);
-        $string     =    str_replace('--', '-', $string);
-
-        return strtr($string, $normalizeChars);
+        return urlencode(iconv(mb_detect_encoding($string), 'ASCII//TRANSLIT//IGNORE', $string));
     }
-
 }
