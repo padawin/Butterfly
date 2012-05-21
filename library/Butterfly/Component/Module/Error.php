a2e3610dbb418756136c89da2868563a08786ed7
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2012-05-21 22:52:02 +0200

    parent class for the error controller, to handle 404 status

---
+++ library/Butterfly/Component/Module/Error.php
@@ -0,0 +1,9 @@
+<?php
+
+class Butterfly_Component_Module_Error extends Butterfly_Component_Module
+{
+    public function notfoundAction()
+    {
+        header('HTTP/1.0 404 Not Found');
+    }
+}
