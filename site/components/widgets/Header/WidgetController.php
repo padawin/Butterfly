852bed46f5eaffddb4ad6c87cc010be6a59041f4
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-01-01 18:29:26 +0100

    needed files and folders created to have a working project

---
+++ site/components/widgets/Header/WidgetController.php
@@ -0,0 +1,9 @@
+<?php
+
+class Header_WidgetController extends Butterfly_Component_Widget
+{
+    public function build()
+    {
+        $this->_view->title = "My new Butterfly project";
+    }
+}
---
+++ www/index-nodb.php
@@ -0,0 +1,35 @@
+<?php
+
+//define the autoload function to load chasses
+function  __autoload($className)
+{
+    $className = implode("/", explode("_", $className)) . '.php';
+    $paths = explode(PATH_SEPARATOR,get_include_path());
+
+    foreach ($paths as $p) {
+        if (file_exists(rtrim($p, '/') . "/$className")) {
+            require $className;
+            return;
+        }
+    }
+
+    throw new Exception("The file $className does not exist");
+}
+
+define('APPLICATION_ENV', 'development');
+
+//define config file and library path
+define('CONFIG_FILE', '../site/config/config.ini');
+define('LIBRARY_PATH', '../library');
+
+//define the include path
+set_include_path(
+LIBRARY_PATH . PATH_SEPARATOR .
+get_include_path());
+
+Butterfly_Factory::setNamespaces(array('%PROJECTNAME%', 'Butterfly'));
+$front = Butterfly_FrontController::getInstance();
+$theme = new Butterfly_Theme;
+$theme->theme_name = 'default';
+$front->setTheme($theme);
+$front->run();
